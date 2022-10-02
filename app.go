fb1db2d95ca065b7d3d9f6682258fb9ab8573073
soumysetu <105278271+soumysetu@users.noreply.github.com>
2022-10-02 16:34:34 +0530

    Update app.go

--- app.go
+++ app.go
@@ -5,9 +5,9 @@ import (
 	"net/http"
 )
 
-func start() {
+func Start() {
 	http.HandleFunc("/greet", greet)
 	http.HandleFunc("/combo", greetall)
-	http.ListenAndServe("localhost:8080", nil)
+	
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
